19e44e018ea01fc1814e85a6e9e7c2026d34799b
Philip Hell <blvckbytes@gmail.com>
2022-02-22 05:01:00 +0100

    Created keyboard draw mode enum

---
+++ include/keyboard_draw_mode.h
@@ -0,0 +1,13 @@
+#ifndef keyboard_draw_mode_h
+#define keyboard_draw_mode_h
+
+#include "util/enumlut.h"
+
+#define _EVALS_KEYBOARD_DRAW_MODE(FUN) \
+  FUN(KDM_RESET_BEFORE,   0x00)        \
+  FUN(AKDM_DD_PREV,       0x01)        \
+  FUN(AKDM_DD_ALL,        0x02)
+
+ENUM_TYPEDEF_FULL_IMPL(keyboard_draw_mode, _EVALS_KEYBOARD_DRAW_MODE);
+
+#endif
---
+++ src/keyboard_draw_mode.c
@@ -0,0 +1,3 @@
+#include "keyboard_draw_mode.h"
+
+ENUM_LUT_FULL_IMPL(keyboard_draw_mode, _EVALS_KEYBOARD_DRAW_MODE);
